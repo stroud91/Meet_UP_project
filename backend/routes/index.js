cf131208572b902e64bcf1a9e283c4e893a44465
Ledian Fekaj <stroud91official@gmail.com>
2023-06-05 21:28:19 -0400

    third push/end of phase 01

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,17 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+
+
+
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -15,5 +21,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
