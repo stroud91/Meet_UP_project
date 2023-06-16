f7a6932bd89361aa062bd4290279325add32051c
Ledian Fekaj <119982061+stroud91@users.noreply.github.com>
2023-06-16 17:00:11 -0400

    Update event-images.js

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -16,60 +16,41 @@ const router = express.Router();
 // Request Method: DELETE
 // URL: /events/:eventId/images/:imageId
 
-router.delete('/api/event-images/:imageId', requireAuth, (req, res) => {
+router.delete('/api/event-images/:imageId', async (req, res) => {
     const imageId = req.params.imageId;
+    const currentUserId = req.user.id; 
 
-    // Fetch the image record
-Image.findOne({
-    where: { id: imageId, imageableType: 'event' },
-    include: [{
-        model: Event,
-        as: 'event',  // Use the correct alias
-        include: [{
-            model: Group,
-            attributes: ['organizerId', 'id']
-        }]
-    }]
-})
+        const image = await Image.findOne({
+            where: { id: imageId, imageableType: 'event' },
+            include: [{
+                model: Event,
+                as: 'event',
+                include: [{
+                    model: Group,
+                    attributes: ['organizerId', 'id']
+                }]
+            }]
+        });
 
-    .then(image => {
-        // Check if image exists
         if (!image) {
             return res.status(404).json({ message: "Event Image couldn't be found" });
         }
 
-        // Check if the current user is an organizer
-        const group = image.imageable.Group;
-        if (req.user.id === group.organizerId) {
-            // User is an organizer, proceed to delete the image
-            return Image.destroy({ where: { id: imageId } });
+        const groupId = image.event.Group.id;
+        const organizerId = image.event.Group.organizerId;
+
+        const membership = await Membership.findOne({
+            where: { groupId: groupId, userId: currentUserId }
+        });
+
+        if (currentUserId !== organizerId && (!membership || membership.status !== 'co-host')) {
+            return res.status(403).json({ message: "You don't have permission to delete this image" });
         }
 
-        // Check if the current user is a co-host
-        return Membership.findOne({
-            where: {
-                userId: req.user.id,
-                groupId: group.id,
-                status: 'co-host'
-            }
-        }).then(membership => {
-            if (membership) {
-                // User is a co-host, proceed to delete the image
-                return Image.destroy({ where: { id: imageId } });
-            } else {
-                // User is neither an organizer nor a co-host
-                return res.status(403).json({ message: "You don't have permission to delete this image" });
-            }
-        });
-    })
-    .then(() => {
-        // Send success response
-        res.status(200).json({ message: 'Successfully deleted' });
-    })
-    .catch(error => {
-        // Handle error
-        res.status(500).json({ message: 'Server Error', error });
-    });
+        await Image.destroy({ where: { id: imageId } });
+
+        return res.status(200).json({ message: 'Successfully deleted' });
+
 });
 
 module.exports = router;
