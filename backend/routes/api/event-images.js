fc2a1f9e1e78ddfd21eb662ec29f16fab407927c
Ledian Fekaj <119982061+stroud91@users.noreply.github.com>
2023-06-16 15:36:22 -0400

    Update event-images.js

--- backend/routes/api/event-images.js
+++ backend/routes/api/event-images.js
@@ -19,35 +19,25 @@ const router = express.Router();
 router.delete('/:imageId', requireAuth, async (req, res, next) => {
     const { imageId } = req.params;
 
-    // Find the image
-    const image = await Image.findOne({
-        where: {
-            imageableId: imageId,
-            imageableType: 'event'
-        }
-    });
+    const image = await Image.findByPk(imageId);
 
-    // If the image doesn't exist, return a 404 error
-    if (!image) {
+    if (!image || image.imageableType !== 'event') {
         return res.status(404).json({ message: "Event Image couldn't be found" });
     }
 
-    // Get the event associated with the image
     const eventId = image.imageableId;
+
     const event = await Event.findByPk(eventId, {
         include: [{
             model: Group,
             attributes: ['organizerId'],
-            as: 'Group'
         }]
     });
 
-    // If the event doesn't exist, return a 404 error
     if (!event) {
         return res.status(404).json({ message: "Event couldn't be found" });
     }
 
-    // Check if the current user is the organizer or a co-host of the group
     const membership = await Membership.findOne({
         where: {
             groupId: event.groupId,
@@ -56,29 +46,17 @@ router.delete('/:imageId', requireAuth, async (req, res, next) => {
     });
 
     const isCohost = membership && membership.status === 'co-host';
-    const isOrganizer = req.user.id === event.Group.organizerId;
+    const isOrganizer = req.user.id === event.organizerId;
 
-    // If the user is neither an organizer nor a co-host, return a 403 error
     if (!isOrganizer && !isCohost) {
         return res.status(403).json({ message: "Unauthorized: Must be the organizer or co-host" });
     }
 
-    // Delete the image
+    
     await image.destroy();
 
-    // Return a successful response
+    
     res.status(200).json({ "message": "Successfully deleted" });
 });
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
