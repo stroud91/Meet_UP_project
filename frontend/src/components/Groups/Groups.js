11ccab473f3d953f1bb4bb22dbc933b2c0acd27a
Ledian Fekaj <stroud91official@gmail.com>
2023-07-25 17:59:43 -0400

    groups route update 1

---
+++ frontend/src/components/Groups/Groups.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getGroups } from '../../store/groups';
+import './Groups.css'
+import GroupDetail from "../GroupDetail/GroupDetail";
+
+
+function Groups() {
+    const dispatch = useDispatch();
+    const groupsData = useSelector(state => state.groups.groups)
+
+    console.log(groupsData)
+    let groupsArray = [];
+    if (groupsData) {
+        groupsArray = Object.values(groupsData);
+    }
+    console.log('groupsArray:', groupsArray);
+    useEffect(() => {
+        dispatch(getGroups())
+    }, [dispatch,]);
+
+    let renderedGroups;
+
+    if (groupsArray.length > 0) {
+        renderedGroups = (
+            groupsArray.map(group => (<GroupDetail key={group.id} group={group} />))
+        )
+    } else {
+        renderedGroups = (
+            <div className="no-groups-display">
+                <img className="no-groups-image" src="https://secure.meetupstatic.com/next/images/home/EmptyGroup.svg?w=384" alt="No groups" />
+                <div className="no-groups-text">Argument text here</div>
+            </div>)
+    }
+
+    return (groupsArray &&
+        <div className="group-ensemble">
+            <div className="links-container">
+                <Link className='groups-link' to='/groups' >Groups</Link>
+                <Link className='events-link' to='/events'>Events</Link>
+            </div>
+            <div className="groups-display">
+                {renderedGroups}
+            </div>
+        </div>
+    )
+}
+export default Groups;
